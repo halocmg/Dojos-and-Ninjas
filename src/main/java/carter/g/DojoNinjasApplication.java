271eaabc87dd5bda9680b2497bee9aad1796b39b
halocmg <halocmg@gmail.com>
2023-01-27 15:19:25 -0700

    Update DojoNinjasApplication.java

--- src/main/java/carter/g/DojoNinjasApplication.java
+++ src/main/java/carter/g/DojoNinjasApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DojoNinjasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DojoNinjasApplication.class, args)s;
+		SpringApplication.run(DojoNinjasApplication.class, args);
 	}
 
 }
